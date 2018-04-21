c799044856d6b2aef6be173be5f9da1f3523538a
icodebr <juniorpcosta@gmail.com>
2018-04-20 22:56:02 -0300

    criando pgadmin e Tpladmin

--- index.php
+++ index.php
@@ -4,19 +4,32 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Icode\Page;
+use \Icode\PageAdmin;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
-    
+//::::::::Rotas:::::::::::::::::
+
+$app->get('/', function() {// Template Index do site
+
 	$page = new Page();
 
 	$page->setTpl("index");
 
 });
 
+
+$app->get('/admin', function() {// Template Admin
+
+	$page = new PageAdmin();
+
+	$page->setTpl("index");
+
+});
+
+
 $app->run();
 
- ?>
+?>
