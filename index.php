8b81be4b9e0f6ca57023a19723c26ec0c787e907
icodebr <juniorpcosta@gmail.com>
2018-04-26 14:43:39 -0300

    cadastro de categorias

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ use \Slim\Slim;
 use \Icode\Page;
 use \Icode\PageAdmin;
 use \Icode\model\User;
+use \Icode\model\Category;
 
 
 $app = new Slim();
@@ -225,6 +226,96 @@ $app->post("/admin/forgot/reset", function(){
 	$page->setTpl("forgot-reset-success");
 
 });
+
+
+$app->get("/admin/categories", function(){
+
+	User::verifyLogin();
+
+	$categories = Category::listALL();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", [
+		"categories"=>$categories
+	]);
+
+});
+
+
+
+$app->get("/admin/categories/create", function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create", function(){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);// pega o array com dados da categoria e seta os mesmos.
+
+    $category->save();
+
+    header("location: /admin/categories");
+    exit;
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header("location: /admin/categories");
+    exit;
+});
+
+
+$app->get("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("/admin/categories-update", [
+
+		"category"=>$category->getValues()
+	]);
+
+});
+
+	$app->post("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);// pega o array com dados da categoria e seta os mesmos.
+
+    $category->save();
+
+    header("location: /admin/categories");
+    exit;
+});
+
 
 
 
